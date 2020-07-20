dba8bf5d500451dfe475a7f687f7d73fea9f34a5
bebinca <bebincaa@163.com>
2020-07-20 05:42:07 -0400

    add map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -43,3 +43,9 @@ function createListElement(text) {
   return liElement;
 }
 
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
